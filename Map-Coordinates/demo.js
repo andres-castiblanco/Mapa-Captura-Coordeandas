38fd608e03cdb2b669510323070008c44501392d
saaperezca <saaperezca@unal.edu.co>
2023-07-28 14:33:42 -0500

    ✨ Actualizacion Repositorio

---
+++ Map-Coordinates/demo.js
@@ -0,0 +1,63 @@
+
+/**
+ * An event listener is added to listen to tap events on the map.
+ * Clicking on the map displays an alert box containing the latitude and longitude
+ * of the location pressed.
+ * @param  {H.Map} map      A HERE Map instance within the application
+ */
+function setUpClickListener(map) {
+  // Attach an event listener to map display
+  // obtain the coordinates and display in an alert box.
+  map.addEventListener('tap', function (evt) {
+    var coord = map.screenToGeo(evt.currentPointer.viewportX,
+            evt.currentPointer.viewportY);
+    logEvent('La coordenadas es: ' 
+    + coord.lat.toFixed(6) + '  ' 
+    + coord.lng.toFixed(6));
+  });
+}
+
+
+
+/**
+ * Boilerplate map initialization code starts below:
+ */
+
+//Step 1: initialize communication with the platform
+// In your own code, replace variable window.apikey with your own apikey
+var platform = new H.service.Platform({
+  apikey: window.apikey
+});
+var defaultLayers = platform.createDefaultLayers();
+
+//Step 2: initialize a map
+var map = new H.Map(document.getElementById('map'),
+  defaultLayers.vector.normal.map,{
+  center: {lat: 4.30246, lng: -72.5811},
+  zoom: 5,
+  pixelRatio: window.devicePixelRatio || 1
+});
+// add a resize listener to make sure that the map occupies the whole container
+window.addEventListener('resize', () => map.getViewPort().resize());
+
+//Step 3: make the map interactive
+// MapEvents enables the event system
+// Behavior implements default interactions for pan/zoom (also on mobile touch environments)
+var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+
+// Step 4: create custom logging facilities
+var logContainer = document.createElement('ul');
+logContainer.className ='log';
+logContainer.innerHTML = '<li class="log-entry">Haga click en la oferta para obtener coordenada</li>';
+map.getElement().appendChild(logContainer);
+
+// Helper for logging events
+function logEvent(str) {
+  var entry = document.createElement('li');
+  entry.className = 'log-entry';
+  entry.textContent = str;
+  logContainer.insertBefore(entry, logContainer.firstChild);
+}
+
+
+setUpClickListener(map);
