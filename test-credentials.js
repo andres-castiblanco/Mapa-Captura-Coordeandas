827b58cf06b91220fe4ebe023bb80f1dad844658
saaperezca <saaperezca@unal.edu.co>
2023-07-30 20:10:29 -0500

    ✨ Actualizacion Repositorio

--- test-credentials.js
+++ test-credentials.js
@@ -1,1 +1,2 @@
-var apikey = 'H6XyiCT0w1t9GgTjqhRXxDMrVj9h78ya3NuxlwM7XUs';
+//var apikey = 'H6XyiCT0w1t9GgTjqhRXxDMrVj9h78ya3NuxlwM7XUs';
+var apikey = 'vO1sA-FyX9VOomF-ziSTlKHJU7PBpjEkPrcMGXPfkQ0';
